4e18d035713d2dab1878470fab8c6e87e6549ded
Wisesofthemall <foxxydieujuste@gmail.com>
2023-04-12 23:40:17 -0400

    Signin Completion with Firebase

---
+++ netflix-ui/src/utils/firebase-config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB4e1a05l8Q-i9-u2PCotv_YEBDM-QkkCM",
+  authDomain: "react-netfloo.firebaseapp.com",
+  projectId: "react-netfloo",
+  storageBucket: "react-netfloo.appspot.com",
+  messagingSenderId: "236476298639",
+  appId: "1:236476298639:web:843ed635445e75a28887fa",
+  measurementId: "G-S6D77RPVL3",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const firebaseAuth = getAuth(app);
